193ec6ed8e5e9ff9c64181ebd53fafe2772c63ac
Thuong Le <thuongle.it@gmail.com>
2016-01-13 13:37:32 +0700

    add main tabs

--- app/src/main/java/com/avectris/curatapp/view/activities/VerifyActivity.java
+++ app/src/main/java/com/avectris/curatapp/view/activities/VerifyActivity.java
@@ -1,18 +1,52 @@
 package com.avectris.curatapp.view.activities;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.avectris.curatapp.R;
+import com.avectris.curatapp.view.widgets.CustomBottomLineEditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by thuongle on 1/13/16.
  */
 public class VerifyActivity extends AppCompatActivity {
+    @Bind(R.id.input_verify_code)
+    CustomBottomLineEditText mInputCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button_verify)
+    public void verify() {
+        if ("1".equals(mInputCode.getText().toString())) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Verify code is invalid")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            builder.create().show();
+        }
     }
 }
